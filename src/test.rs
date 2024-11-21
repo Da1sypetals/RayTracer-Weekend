e16b8ad88b26bc4b1d0e7cf6a0327929bf271289
da1sy petals <11695641+da1sypetals@user.noreply.gitee.com>
2024-11-22 01:05:25 +0800

    jnbh

--- src/test.rs
+++
@@ -1,8 +0,0 @@
-use raytrace::config::materials::MaterialMap;
-
-fn main() {
-    let mats = MaterialMap::configured("config/materials.toml").unwrap();
-    mats.map
-        .into_iter()
-        .for_each(|(name, m)| println!("{} => {:?}", name, m));
-}
